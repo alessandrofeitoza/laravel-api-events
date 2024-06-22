b9af8c7cb77ad21a14a9c0293ddd5f9e6a3751fe
Paulo Victor <dev.paulovictor@gmail.com>
2024-06-22 17:36:49 -0300

    Ajusta a execucao dos seeders

--- database/seeders/RoomTypeSeeder.php
+++ database/seeders/RoomTypeSeeder.php
@@ -29,8 +29,10 @@ class RoomTypeSeeder extends Seeder
 
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table(Room::getTableName())->truncate();
         DB::table(RoomType::getTableName())->delete();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         foreach (self::VALUES as $item) {
             $object = new RoomType();
