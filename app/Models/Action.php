adfad771220155a4f6c7dbd2857aa86b2ab66f3a
TalysonSoares <talysonsoares40@gmail.com>
2024-07-11 20:18:02 -0300

    Create new entity action

---
+++ app/Models/Action.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Action extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+    ];
+
+    public static function getTableName(): string
+    {
+        return with(new self)->getTable();
+    }
+}
