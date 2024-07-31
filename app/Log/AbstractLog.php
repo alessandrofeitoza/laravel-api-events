086e655ad1c59baa732b6c058440acab992c3344
Gabriela Lobo <gabriela.lobomartins@gmail.com>
2024-07-30 22:14:18 -0300

    PHPC dedurando as outras paginas quebradas

--- app/Log/AbstractLog.php
+++ app/Log/AbstractLog.php
@@ -15,9 +15,9 @@ abstract class AbstractLog
 
     public function store(): void
     {
-        $file = fopen(dirname(__DIR__, 2)."/logs/{$this->type}.csv", 'a+');
+        $file = fopen(dirname(__DIR__, 2) . "/logs/{$this->type}.csv", 'a+');
 
-        $content = $this->title . ";" .date('Y-m-d H:i:s').";{$this->content}";
+        $content = $this->title . ";" . date('Y-m-d H:i:s') . ";{$this->content}";
 
         fwrite($file, $content . PHP_EOL);
     }
