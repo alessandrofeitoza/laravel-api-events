d502daef2da164a9ded1d80785a1b6874010e5b9
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-08-01 17:09:19 -0300

    Add flash messages on admin pages

--- app/Http/Controllers/Admin/RoomTypeAdminController.php
+++ app/Http/Controllers/Admin/RoomTypeAdminController.php
@@ -24,16 +24,27 @@ class RoomTypeAdminController extends Controller
 
     public function store(Request $request): mixed
     {
-        if (true === $request->isMethod('post')) {
-            $object = new RoomType();
-            $object->name = $request->input('name');
-            $object->description = $request->input('description');
-
-            $object->save();
-
-            return redirect(self::BASE_URL);
+        if (false === $request->isMethod('post')) {
+            return view(self::VIEW_BASE_PATH . 'add');
         }
 
-        return view(self::VIEW_BASE_PATH . '/add');
+        $exists = RoomType::where('name', $request->input('name'))->first();
+
+        if (null !== $exists) {
+            $request->session()->flash('error', 'Tipo de sala já existe');
+
+            return view(self::VIEW_BASE_PATH . 'add');
+        }
+
+        $object = new RoomType();
+        $object->name = $request->input('name');
+        $object->description = $request->input('description');
+
+        $object->save();
+
+        $request->session()->flash('success', 'Novo tipo de sala inserido');
+
+        return redirect(self::BASE_URL);
+    
     }
 }
